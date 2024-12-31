223e2b04f19f73dc689e3b604033ea185032c6e3
alisha-tamboli <tambolialisha2003@gmail.com>
2025-01-01 00:56:07 +0530

    file updated

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://my-mern-portfolio-dzlodlfq8-alisha-tambolis-projects.vercel.app', // Add your frontend URL here
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // MongoDB setup
 // mongoose.connect('mongodb://localhost:27017/Portfolio_db')
